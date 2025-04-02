8ffa073af21b7965c9f64d1cfd653fc13647973d
Debora Serra <debora.r.serra@gmail.com>
2025-04-02 12:31:13 -0700

    feat: add colors built by sd

---
+++ src/sd/build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,38 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let colorBlue100 = #e3ebfc
+    public static let colorBlue200 = #cdd9f8
+    public static let colorBlue300 = #b5c6f4
+    public static let colorBlue400 = #8d9fec
+    public static let colorBlue50 = #f0f4fd
+    public static let colorBlue500 = #727fe2
+    public static let colorBlue600 = #575cd4
+    public static let colorBlue700 = #4749bb
+    public static let colorBlue800 = #3c4097
+    public static let colorBlue900 = #373b78
+    public static let colorBlue950 = #202246
+    public static let colorGray0 = #ffffff
+    public static let colorGray10 = #f4f4ff
+    public static let colorGray100 = #010314
+    public static let colorGray20 = #ececfb
+    public static let colorGray30 = #dfe1f4
+    public static let colorGray40 = #babcd2
+    public static let colorGray50 = #9fa2b9
+    public static let colorGray60 = #5e6077
+    public static let colorGray70 = #383a4d
+    public static let colorGray80 = #2a2b3a
+    public static let colorGray85 = #212230
+    public static let colorGray90 = #171825
+    public static let colorGray95 = #0b0d1c
+    public static let fontBody = Inter, sans-serif
+    public static let fontMono = Menlo, monospace
+}
---
+++ src/sd/build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,38 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let colorBlue100 = #e3ebfc
+    internal static let colorBlue200 = #cdd9f8
+    internal static let colorBlue300 = #b5c6f4
+    internal static let colorBlue400 = #8d9fec
+    internal static let colorBlue50 = #f0f4fd
+    internal static let colorBlue500 = #727fe2
+    internal static let colorBlue600 = #575cd4
+    internal static let colorBlue700 = #4749bb
+    internal static let colorBlue800 = #3c4097
+    internal static let colorBlue900 = #373b78
+    internal static let colorBlue950 = #202246
+    internal static let colorGray0 = #ffffff
+    internal static let colorGray10 = #f4f4ff
+    internal static let colorGray100 = #010314
+    internal static let colorGray20 = #ececfb
+    internal static let colorGray30 = #dfe1f4
+    internal static let colorGray40 = #babcd2
+    internal static let colorGray50 = #9fa2b9
+    internal static let colorGray60 = #5e6077
+    internal static let colorGray70 = #383a4d
+    internal static let colorGray80 = #2a2b3a
+    internal static let colorGray85 = #212230
+    internal static let colorGray90 = #171825
+    internal static let colorGray95 = #0b0d1c
+    internal static let fontBody = Inter, sans-serif
+    internal static let fontMono = Menlo, monospace
+}
