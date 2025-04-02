3073808d358c6d90b4edfa0aa200baee9b924288
Debora Serra <debora.r.serra@gmail.com>
2025-04-02 12:29:59 -0700

    chore: update colors and add tokens file

--- src/style/colors.ts
+++ src/style/colors.ts
@@ -1,37 +1,17 @@
 export const colors = {
-  zinc: {
-    100: '#E2E2E9', //text
-    200: '#dce2f9', //text link
-    300: '#c5c6d0', //text
-    400: '#8F9099',
-    500: '#404659', // bg btn secondary
-    600: '#44464f', //line header
-    700: '#38393F',
-    800: '#33353A', // bg graph
-    900: '#1e1f25', //bg secondary
-    950: '#121318', //gb primary
-  },
-
-  purple: {
-    200: '#FDD7FA',
-    700: '#593D5A', // bg button try
-    800: '#592c4f', // bg button primary
-  },
-
-  green: {
-    500: '#3CB66D', //badge
-    600: '#1DA253',
-  },
-
   blue: {
-    200: '#D9E2FF', // checkbox check
-    300: '#B5C6F4', // bg button primary
-    400: '#a0bfff', // bg button primary
-    500: '#2E4578', // checkbox bg
-    600: '#152E60', // btn primary text
-    900: '#282A2F',
+    '50': '#f0f4fd',
+    '100': '#e3ebfc',
+    '200': '#cdd9f8',
+    '300': '#b5c6f4',
+    '400': '#8d9fec',
+    '500': '#727fe2',
+    '600': '#575cd4',
+    '700': '#4749bb',
+    '800': '#3c4097',
+    '900': '#373b78',
+    '950': '#202246',
   },
-
   gray: {
     100: '#010314',
     95: '#0B0D1C',
---
+++ src/style/tokens.js
@@ -0,0 +1,34 @@
+const theme = {
+  color: {
+    'blue-50': { value: '#f0f4fd' },
+    'blue-100': { value: '#e3ebfc' },
+    'blue-200': { value: '#cdd9f8' },
+    'blue-300': { value: '#b5c6f4' },
+    'blue-400': { value: '#8d9fec' },
+    'blue-500': { value: '#727fe2' },
+    'blue-600': { value: '#575cd4' },
+    'blue-700': { value: '#4749bb' },
+    'blue-800': { value: '#3c4097' },
+    'blue-900': { value: '#373b78' },
+    'blue-950': { value: '#202246' },
+    'gray-0': { value: '#ffffff' },
+    'gray-10': { value: '#f4f4ff' },
+    'gray-20': { value: '#ececfb' },
+    'gray-30': { value: '#dfe1f4' },
+    'gray-40': { value: '#babcd2' },
+    'gray-50': { value: '#9fa2b9' },
+    'gray-60': { value: '#5e6077' },
+    'gray-70': { value: '#383a4d' },
+    'gray-80': { value: '#2a2b3a' },
+    'gray-85': { value: '#212230' },
+    'gray-90': { value: '#171825' },
+    'gray-95': { value: '#0b0d1c' },
+    'gray-100': { value: '#010314' },
+  },
+  font: {
+    'body': { value: 'Inter, sans-serif' },
+    'mono': { value: 'Menlo, monospace' },
+  }
+};
+
+export default theme;
