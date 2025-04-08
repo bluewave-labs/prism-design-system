8fb019ebf0d46ff079b7ea858b4e698db25ce09d
Debora Serra <debora.r.serra@gmail.com>
2025-04-08 13:53:52 -0700

    chore: export card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,3 @@
+export type { CardProps } from '../../types/card';
+export { CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, ConsoleButton } from '../ui/card';
+export { Card } from './card';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './Button';
+export * from './Card';
 export * from './Sidebar';
 export * from './ui/avatar';
 export * from './ui/collapsible';
