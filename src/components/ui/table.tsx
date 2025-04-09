d4fca0252bb19e52ac2eaf2d2744655ce26d83f9
Debora Serra <debora.r.serra@gmail.com>
2025-04-09 15:55:15 -0700

    chore: add coderabbit suggestion

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -4,13 +4,15 @@ import * as React from 'react';
 
 import { cn } from '../../lib/utils';
 
-function Table({ className, ...props }: React.ComponentProps<'table'>) {
+function Table({ className, children, ...props }: React.ComponentProps<'table'>) {
   return (
     <div
       data-slot="table-container"
       className={cn('relative w-full overflow-x-auto  border-[0.5px] border-gray-0/20 rounded-xl', className)}
     >
-      <table data-slot="table" className={cn('w-full caption-bottom text-sm')} {...props} />
+      <table data-slot="table" className={cn('w-full caption-bottom text-sm')} {...props}>
+        {children}
+      </table>
     </div>
   );
 }
