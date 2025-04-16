1d4b23bb0b869af27a0180dfc25b17afc0f3d1da
Debora Serra <debora.r.serra@gmail.com>
2025-04-16 09:53:50 -0700

    fix: fix sidebar rendering

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -227,7 +227,7 @@ function Sidebar({
 
   return (
     <div
-      className="group peer hidden md:block"
+      className="group peer block"
       data-state={state}
       data-collapsible={state === 'collapsed' ? collapsible : ''}
       data-variant={variant}
